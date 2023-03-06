8d11b6f338b3d13354d8d347c7e6090caf3d6e78
darkfox57 <andres.pinto.n@gmail.com>
2023-03-05 21:48:09 -0500

    funcionalidad

--- client/src/components/Filters/Filters.jsx
+++ client/src/components/Filters/Filters.jsx
@@ -1,40 +1,141 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+  filterByGenres,
+  filterByOrigin,
+  getGenres,
+  orderNames,
+  orderRating,
+} from '../../redux/actions/actions'
 import styles from './filters.module.css'
 
 export default function Filters() {
-  const selectOption = [
+  // obtener y usar generos
+  const genres = useSelector((state) => state.genres)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(getGenres())
+  }, [dispatch])
+
+  const filterOrder = [
     {
       id: 1,
       value: 'Ascendent',
       label: 'Ascendent',
     },
-    { id: 2, value: 'Descendent', label: 'Descendent' },
+    {
+      id: 2,
+      value: 'Descendent',
+      label: 'Descendent',
+    },
   ]
 
-  const handleClick = (e) => {
+  const filterOrigin = [
+    {
+      id: 1,
+      value: 'Api',
+      label: 'Api',
+    },
+    {
+      id: 2,
+      value: 'Created',
+      label: 'Created',
+    },
+  ]
+
+  const handleFilter = (e) => {
     e.preventDefault()
     const { value, name } = e.target
-    if (name === 'filter') {
-      return dispatch(filterCards(value))
+    if (name === 'filterByGenres') {
+      dispatch(filterByGenres(value))
+      document.getElementsByName('orderByName')[0].value = 'Default'
+      document.getElementsByName('orderByRating')[0].value = 'Default'
+      document.getElementsByName('Origin')[0].value = 'Default'
     }
-    if (name === 'order') {
-      return dispatch(orderCards(value))
+    if (name === 'orderByName') {
+      dispatch(orderNames(value))
+      document.getElementsByName('filterByGenres')[0].value = 'Default'
+      document.getElementsByName('orderByRating')[0].value = 'Default'
+      document.getElementsByName('Origin')[0].value = 'Default'
+    }
+    if (name === 'orderByRating') {
+      dispatch(orderRating(value))
+      document.getElementsByName('filterByGenres')[0].value = 'Default'
+      document.getElementsByName('orderByName')[0].value = 'Default'
+      document.getElementsByName('Origin')[0].value = 'Default'
+    }
+    if (name === 'Origin') {
+      dispatch(filterByOrigin(value))
+      document.getElementsByName('filterByGenres')[0].value = 'Default'
+      document.getElementsByName('orderByName')[0].value = 'Default'
+      document.getElementsByName('orderByRating')[0].value = 'Default'
     }
   }
+
   return (
     <section className={styles.filterSection}>
       <div>Filters</div>
       <div className={styles.selectors}>
-        <select name="order" onChange={handleClick} defaultValue={'default'}>
-          <option value="default" disabled>
-            Select option ...
-          </option>
-          {selectOption.map((element) => (
-            <option key={element.id} value={element.value}>
-              {element.label}
-            </option>
-          ))}
-        </select>
+        <div className={styles.orderName}>
+          <p>Order by name</p>
+          <select
+            name="orderByName"
+            defaultValue={'default'}
+            onChange={handleFilter}
+          >
+            <option value="Default">--</option>
+            {filterOrder.map((element) => (
+              <option value={element.value} key={element.id}>
+                {element.value}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className={styles.orderRating}>
+          <p>Order by Rating</p>
+          <select
+            name="orderByRating"
+            defaultValue={'default'}
+            onChange={handleFilter}
+          >
+            <option value="Default">--</option>
+            {filterOrder.map((element) => (
+              <option value={element.value} key={element.id}>
+                {element.value}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className={styles.orderGender}>
+          <p>Filter by Gender</p>
+          <select
+            name="filterByGenres"
+            defaultValue={'default'}
+            onChange={handleFilter}
+          >
+            <option value="Default">--</option>
+            {genres.map((genre) => (
+              <option key={genre.id} value={genre.name}>
+                {genre.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className={styles.filterOrigin}>
+          <p>Filter by Origin</p>
+          <select
+            name="Origin"
+            defaultValue={'default'}
+            onChange={handleFilter}
+          >
+            <option value="Default">--</option>
+            {filterOrigin.map((o) => (
+              <option key={o.id} value={o.value}>
+                {o.value}
+              </option>
+            ))}
+          </select>
+        </div>
       </div>
     </section>
   )
